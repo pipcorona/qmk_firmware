01d72b5ff20fbb7fca7b6bd3b4d28a9c057df776
pipcorona <pipcorona@gmail.com>
2020-06-20 01:11:00 -0700

    Update keymap.c

--- keyboards/planck/keymaps/pipcorona/keymap.c
+++ keyboards/planck/keymaps/pipcorona/keymap.c
@@ -3,22 +3,6 @@
 #include "muse.h"
 #endif
 #include "eeprom.h"
-#include "keymap_german.h"
-#include "keymap_nordic.h"
-#include "keymap_french.h"
-#include "keymap_spanish.h"
-#include "keymap_hungarian.h"
-#include "keymap_swedish.h"
-#include "keymap_br_abnt2.h"
-#include "keymap_canadian_multilingual.h"
-#include "keymap_german_ch.h"
-#include "keymap_jp.h"
-#include "keymap_bepo.h"
-#include "keymap_italian.h"
-#include "keymap_slovenian.h"
-#include "keymap_danish.h"
-#include "keymap_norwegian.h"
-#include "keymap_portuguese.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
